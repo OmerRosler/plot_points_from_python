27aed4fd59db15db10f0d9983c9db117c26d1540
Omer Rosler <omer.rosler@gmail.com>
2023-02-04 22:14:34 +0200

    Changed the file format to include the k value used as the python program now iterates k as well

--- plot_points_from_python.cpp
+++ plot_points_from_python.cpp
@@ -18,11 +18,9 @@ struct image_data_t : image_metadata_t
 
 }
 
-struct reader_state_machine
+class reader_state_machine
 {
-    frc::image_data_t& m_img;
-    std::string m_polynomial;
-    rgb_t m_color;
+public:
     reader_state_machine(frc::image_data_t& img):
         m_img(img),
         m_polynomial(),
@@ -32,7 +30,7 @@ struct reader_state_machine
     void read_poly(std::wstring m_polynomial)
     {
         //TODO: Input this number from the python program
-        constexpr auto num_of_polys = 5;
+        constexpr auto num_of_polys = 17;
         static auto color_ind = 0;
         color_ind++;
         constexpr int jump_size = 255 / (num_of_polys + 1);
@@ -43,12 +41,21 @@ struct reader_state_machine
             unsigned char(color_v / 7), 
             unsigned char(color_v / 11) };
     }
+
+    void read_k(int k)
+    {
+        m_k = k;
+    }
     void set_pixel_of_point(double x, double y)
     {
         auto [pixel_x, pixel_y] = m_img.pixel_id_from_value(x, y);
-        //The formula is the inverse of the one used to convert the index to the point in `run_algorithm_per_pixel`
         m_img.img.set_pixel(pixel_x, pixel_y, m_color);
     }
+private:
+    frc::image_data_t& m_img;
+    std::string m_polynomial;
+    int m_k;
+    rgb_t m_color;
 };
 std::wistream& read_file(std::wistream& is, frc::image_data_t& img)
 {
@@ -58,8 +65,16 @@ std::wistream& read_file(std::wistream& is, frc::image_data_t& img)
     {
         if (line == L"STOP")
         {
+            // Read the new polynomial
             std::getline(is, line);
             sm.read_poly(line);
+            // Read the `k` value used for the trap
+            std::getline(is, line);
+            std::wstringstream line_stream(line);
+            int k;
+            line_stream >> k;
+            sm.read_k(k);
+            // Start reading points
             continue;
         }
         std::wstringstream line_stream(line);
